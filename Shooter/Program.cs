2a7f02fc5794212fe92aa0c7e1b7fd181dd12d8f
Rookin_work <crazynick21@gmail.com>
2023-02-01 13:18:42 +0300

    Добавлена карта,  добавлена невозможность проходить сквозь стены

--- Shooter/Program.cs
+++ Shooter/Program.cs
@@ -1,4 +1,7 @@
-﻿namespace Shooter
+﻿using System.Reflection.Emit;
+using System.Text;
+
+namespace Shooter
 {
     class Program
     {
@@ -16,7 +19,7 @@
         private static double _PlayerY = 5;
         private static double _PlayerA = 0; // Угол, под которым смотрит игрок
 
-        private static string _Map = ""; // Для хранения самой карты
+        private static readonly StringBuilder _Map = new StringBuilder();
 
         // Массив символов, который будет обновляться каждую итерацию
         private static readonly char[] _Screen = new char[_ScreenWidth * _ScreenHeight];
@@ -27,39 +30,7 @@
             Console.SetBufferSize(_ScreenWidth, _ScreenHeight); // Установка размера буфера
             Console.CursorVisible = false; // Выключение курсора
 
-            // Стена 
-            _Map += "#################################"; // Верх стены
-            _Map += "#...............................#"; // Точки пустое пространство
-            _Map += "#...............................#";
-            _Map += "#...............................#";
-            _Map += "#...............................#";
-            _Map += "#...............................#";
-            _Map += "#...............................#";
-            _Map += "#...............................#";
-            _Map += "#...............................#";
-            _Map += "#...............................#";
-            _Map += "#...............................#";
-            _Map += "#..###..........................#";
-            _Map += "#...............................#";
-            _Map += "#...............................#";
-            _Map += "#...............................#";
-            _Map += "#...............................#";
-            _Map += "#...............................#";
-            _Map += "#...............................#";
-            _Map += "#...............................#";
-            _Map += "#...............................#";
-            _Map += "#...............................#";
-            _Map += "#...............................#";
-            _Map += "#...............................#";
-            _Map += "#...............................#";
-            _Map += "#...............................#";
-            _Map += "#...............................#";
-            _Map += "#...............................#";
-            _Map += "#...............................#";
-            _Map += "#...............................#";
-            _Map += "#...............................#";
-            _Map += "#...............................#";
-            _Map += "#################################"; // Низ стены
+            InitMap();
 
             DateTime DateTimeFrom = DateTime.Now; // Время добавлено для того, чтобы не дергалась анимация           
 
@@ -69,8 +40,47 @@
                 double ElapsedTime = (DateTimeTo - DateTimeFrom).TotalSeconds;
                 DateTimeFrom = DateTime.Now;
 
-                // Каждую итерацию добавляем угол
-                _PlayerA += 0.5 * ElapsedTime;
+                if(Console.KeyAvailable)
+                {
+                    // Считывание с клавиатуры для движения
+                    ConsoleKey consoleKey = Console.ReadKey(intercept: true).Key; 
+
+                    switch (consoleKey)
+                    {
+                        case ConsoleKey.A: // Движение влево
+                            _PlayerA += ElapsedTime * 2;
+                            break; 
+                        case ConsoleKey.D: // Движение вправо
+                            _PlayerA -= ElapsedTime * 2;
+                            break;
+                        case ConsoleKey.W: // Движение прямо
+                            {
+                                _PlayerX += Math.Sin(_PlayerA) * 10 * ElapsedTime;
+                                _PlayerY += Math.Cos(_PlayerA) * 10 * ElapsedTime;
+
+                                // Если уперлись в стену, то делаем шаг назад
+                                if (_Map[(int)_PlayerY * _MapWidth + (int)_PlayerX] == '#')
+                                {
+                                    _PlayerX -= Math.Sin(_PlayerA) * 10 * ElapsedTime;
+                                    _PlayerY -= Math.Cos(_PlayerA) * 10 * ElapsedTime;
+                                }
+                                break;
+                            }
+                        case ConsoleKey.S: // Движение назад
+                            {
+                                _PlayerX -= Math.Sin(_PlayerA) * 10 * ElapsedTime;
+                                _PlayerY -= Math.Cos(_PlayerA) * 10 * ElapsedTime;
+
+                                // Если уперлись в стену, то делаем шаг впеед
+                                if (_Map[(int)_PlayerY * _MapWidth + (int)_PlayerX] == '#')
+                                {
+                                    _PlayerX += Math.Sin(_PlayerA) * 10 * ElapsedTime;
+                                    _PlayerY += Math.Cos(_PlayerA) * 10 * ElapsedTime;
+                                }
+                                break;
+                            }
+                    }
+                }
 
                 for (int x = 0; x < _ScreenWidth; x++)
                 {
@@ -82,6 +92,7 @@
 
                     double DistanceToWall = 0; // Дистанция до стены
                     bool HitWall = false; // Попали в стену или нет
+                    bool IsBound = false; // Попадает в угол или нет
 
                     while (!HitWall && DistanceToWall < _Depth) // Пока не попали в стену
                     {
@@ -104,6 +115,41 @@
                             if (TestCell == '#')
                             {
                                 HitWall = true;
+
+                                // Отрисовка граней стены
+                                var BoundVecorList = new List<(double module, double cos)>(); 
+                                
+                                // Проход по 4 ребрам
+                                for(int tx = 0; tx < 2; tx++)
+                                {
+                                    for (int ty = 0; _PlayerY < 2; ty++)
+                                    {
+                                        // Подсчет x и y объекта
+                                        double vx = TestX + tx - _PlayerX;
+                                        double vy = TestY + ty - _PlayerY;
+
+                                        // Подсчет модуль вектора
+                                        double VectorModule = Math.Sqrt(vx * vx + vy * vy);
+                                        // Подсчет косинуса угла
+                                        double CosAngle = RayX * vx / VectorModule + RayY * vy / VectorModule;  
+
+                                        BoundVecorList.Add((VectorModule, CosAngle)); // Добавление списка
+                                    }
+                                }
+                                // Сортировка списка по модулю, чтобы первые были два самых ближайших вектора
+                                BoundVecorList = BoundVecorList.OrderBy(v => v.module).ToList();
+
+                                // Деление нужно для того, чтобы издали грани отображались корректно
+                                double BoundAngle = 0.03 / DistanceToWall; 
+
+                                // Проверка для нулевого и первого элемента, попадает ли угол в константу
+                                if (Math.Acos(BoundVecorList[0].cos) < BoundAngle ||
+                                    Math.Acos(BoundVecorList[1].cos) < BoundAngle)
+                                    IsBound = true;
+                            }
+                            else
+                            {
+                                _Map[TestY * _MapWidth + TestX] = '*';
                             }
                         }
                     }
@@ -113,26 +159,18 @@
 
                     char WallShade; // Окрас стены
 
-                    if (DistanceToWall < _Depth / 4d)
-                    {
+                    if (IsBound)
+                        WallShade = '|';
+                    else if (DistanceToWall < _Depth / 4d)
                         WallShade = '\u2588';
-                    }
                     else if (DistanceToWall < _Depth / 3d)
-                    {
                         WallShade = '\u2593';
-                    }
                     else if (DistanceToWall < _Depth / 2d)
-                    {
                         WallShade = '\u2592';
-                    }
                     else if (DistanceToWall < _Depth)
-                    {
                         WallShade = '\u2591';
-                    }
                     else
-                    {
                         WallShade = ' ';
-                    }
 
                     // Цикл, который проходит по высоте экрана
                     for (int y = 0; y < _ScreenHeight; y++)
@@ -143,18 +181,88 @@
                         }
                         else if (y > Ceiling && y <= Floor) // Если больше, чем потолок и меньше, чем пол, то это стена
                         {
-                            _Screen[y * _ScreenWidth + x] = WallShade; // Отрисовываем стену
+                            _Screen[y * _ScreenWidth + x] = WallShade; // Отрисовка стены
                         }
-                        else
+                        else // Декорирование пола. Взависимости от приближения
                         {
-                            _Screen[y * _ScreenWidth + x] = '.'; // Отрисовываем пол
+                            char FloorShade;
+
+                            double b = 1 - (y - _ScreenHeight / 2d) / (_ScreenHeight / 2d);
+
+                            if (b < 0.25)
+                                FloorShade = '#';
+                            else if (b < 0.5)
+                                FloorShade = 'x';
+                            else if (b < 0.75)
+                                FloorShade = '-';
+                            else if (b < 0.9)
+                                FloorShade = '.';
+                            else
+                                FloorShade = ' ';
+
+                            _Screen[y * _ScreenWidth + x] = FloorShade; // Отрисововка пола
                         }
                     }
                 }
 
+                // Вывод информации в углу экрана
+                char[] stats = $"X: {_PlayerX}, Y: {_PlayerY}, A: {_PlayerA}, FPS: {(int)(1 / ElapsedTime)}"
+                    .ToArray();
+                stats.CopyTo(array:_Screen, index: 0);
+
+                // Вывод карты
+                for (int x = 0; x < _MapWidth; x++)
+                {
+                    for (int y = 0; y < _MapHeight; y++)
+                    {
+                        _Screen[(y + 1) * _ScreenWidth + x] = _Map[y * _MapWidth+ x];
+                    }
+                }
+
+                // Положение игрока на карте
+                _Screen[(int)(_PlayerY + 1) * _ScreenWidth + (int)_PlayerX] = 'P';
+
                 Console.SetCursorPosition(left: 0, top: 0);
                 Console.Write(buffer: _Screen);
             }
+        }
+
+        private static void InitMap()
+        {
+            _Map.Clear();
+            // Стена 
+            _Map.Append("#################################"); // Верх стены
+            _Map.Append("#....................#..........#"); // Точки пустое пространство
+            _Map.Append("#....................#..........#");
+            _Map.Append("#....................#..........#");
+            _Map.Append("#....................#..........#");
+            _Map.Append("#....................#..........#");
+            _Map.Append("#....................#..........#");
+            _Map.Append("#....................#..........#");
+            _Map.Append("#......######........#..........#");
+            _Map.Append("#....................#..........#");
+            _Map.Append("#...............................#");
+            _Map.Append("#...............................#");
+            _Map.Append("#.....................###########");
+            _Map.Append("#........#......................#");
+            _Map.Append("#...............................#");
+            _Map.Append("#...............................#");
+            _Map.Append("#.........########..............#");
+            _Map.Append("#...............................#");
+            _Map.Append("#...............................#");
+            _Map.Append("#...............................#");
+            _Map.Append("#...............................#");
+            _Map.Append("#...............................#");
+            _Map.Append("###########.....................#");
+            _Map.Append("#...............................#");
+            _Map.Append("#.........#.....................#");
+            _Map.Append("#.........#.....................#");
+            _Map.Append("#.........#.....................#");
+            _Map.Append("#.........#.....................#");
+            _Map.Append("#.........#.....................#");
+            _Map.Append("#.........#.....................#");
+            _Map.Append("#.........#.....................#");
+            _Map.Append("#################################"); // Низ стены
         }
     }
 }
